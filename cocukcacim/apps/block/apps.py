26cdae94d6416442c58cd4d2d37a8309210fb03a
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-11-27 18:06:00 +0300

    start block app

---
+++ cocukcacim/apps/block/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlockConfig(AppConfig):
+    name = 'cocukcacim.apps.block'
+    verbose_name = "İçerik Bloğu"
---
+++ cocukcacim/apps/block/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class BlockPosition(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Güncelleme Tarihi")
+    name = models.CharField(max_length=255, verbose_name="Pozisyon İsmi")
+    position_name_slug = models.CharField(max_length=60, verbose_name="Pozisyon IDsi")
+
+    # Primary Key veya ID
+
+    # Sınıfla ilgili bilgiler
+    class Meta:
+        verbose_name = "Blok Pozisyonu"
+        verbose_name_plural = "Blok Pozisyonları"
+        ordering = ['created_at']
+
+    # classın override ettiğimiz fonksiyonları
+    def __str__(self):
+        return self.title
+
+
+class Block(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Güncelleme Tarihi")
+    name = models.CharField(max_length=255, verbose_name="İsim", default="Blok İsmi Giriniz!!!!")
+    title1 = models.CharField(max_length=255, verbose_name="Başlık 1", blank=True, null=True)
+    title2 = models.CharField(max_length=255, verbose_name="Başlık 2", blank=True, null=True)
+    icon = models.CharField(max_length=255, verbose_name="Font Awesome Icon İsmi", blank=True, null=True)
+    description = models.TextField(verbose_name="Açıklama", null=True, blank=True)
+    image1 = models.TextField(verbose_name="Resim 1", null=True, blank=True)
+    image2 = models.TextField(verbose_name="Resim 2", blank=True, null=True)
+
+
+    # Sınıfla ilgili bilgiler
+    class Meta:
+        verbose_name = "Blok"
+        verbose_name_plural = "Bloklar"
+        ordering = ['created_at']
+
+    # classın override ettiğimiz fonksiyonları
+    def __str__(self):
+        return self.title
