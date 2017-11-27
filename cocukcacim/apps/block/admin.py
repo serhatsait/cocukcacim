ed729ad707af1b9a7abf04ec71e9721cdf4ebf7a
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-11-27 18:23:57 +0300

    block app admin features

--- cocukcacim/apps/block/admin.py
+++ cocukcacim/apps/block/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from cocukcacim.apps.block.models import BlockPosition, Block
 
-# Register your models here.
+admin.site.register(BlockPosition)
+
+
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ('title1', 'name', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at', 'title1', 'name')
+    search_fields = ('title1', 'title2', 'name',)
+
+
+admin.site.register(Block, BlockAdmin)
